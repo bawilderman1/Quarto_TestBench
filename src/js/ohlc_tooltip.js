bd28192654f82ff2904b277fc8db72247df132f7
bawilderman1 <30907764+bawilderman1@users.noreply.github.com>
2025-05-22 23:25:04 -0500

    Still in progress of refactoring backtest chart (WIP)

--- src/js/ohlc_tooltip.js
+++ src/js/ohlc_tooltip.js
@@ -1,6 +1,5 @@
 function () {
-  return '<span style="color:' + this.series.lineColor + '">●</span> <b>' + this.series.name + '</b><br/>' +
-       'Open: <b>' + new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(this.open) + '</b> <br/>' +
+  return 'Open: <b>' + new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(this.open) + '</b> <br/>' +
        'High: <b>' + new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(this.high) + '</b> <br/>' +
        'Low: <b>' + new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(this.low) + '</b> <br/>' +
        'Close: <b>' + new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(this.close) + '</b> <br/>';  
